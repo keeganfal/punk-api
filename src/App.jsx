b5da90f1e46ac23544267369dba87481c6e41a60
keeganfal <60140681+keeganfal@users.noreply.github.com>
2022-07-24 19:20:31 +0100

    gh pages deployment added

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+
+import './App.css';
+import Nav from './components/Nav/Nav.jsx';
+
+function App() {
+  return (
+    <div className="App">
+      <Nav></Nav>
+
+
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Nav/Nav.jsx
@@ -0,0 +1,11 @@
+
+const Nav = props => {
+
+  return (
+    <nav className="nav">
+      <h2 className="nav__heading">Punk API</h2>
+    </nav>
+  );
+};
+
+export default Nav;
