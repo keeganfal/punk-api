165a5c61e9f92026e0e2aac38e32b7f3a674567f
keeganfal <60140681+keeganfal@users.noreply.github.com>
2022-08-01 02:37:28 +0100

    Filters Added

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@
 import './App.css';
 import CardList from './components/CardList/CardList';
 import SearchBar from './components/SearchBar/SearchBar.jsx';
+import FiltersList from './components/FiltersList/FiltersList';
 
 import { useState } from 'react';
 
@@ -26,8 +27,42 @@ function App() {
     const res = await fetch("https://api.punkapi.com/v2/beers");
     const data = await res.json();
     console.log(data);
-    const newStr = data.filter((beer) => {
+    const newArr = data.filter((beer) => {
         if (beer.name === beerName) {
+          return(setBeers(<CardList key={beer.id} beerName={beer.name} beerImg={beer.image_url} beerTag={beer.tagline}/>))
+        };
+        return(null)
+      })
+  };
+
+  const handleHighAlcohol = async () => {
+    const res = await fetch("https://api.punkapi.com/v2/beers?abv_gt=6");
+    const data = await res.json();
+
+    const cardListJSX = data.map((beer) => (
+      <CardList key={beer.id} beerName={beer.name} beerImg={beer.image_url} beerTag={beer.tagline}/>
+    ));
+
+    setBeers(cardListJSX)
+  }
+
+  const handleClassicRange = async () => {
+    const res = await fetch("https://api.punkapi.com/v2/beers?brewed_before=10");
+    const data = await res.json();
+
+    const cardListJSX = data.map((beer) => (
+      <CardList key={beer.id} beerName={beer.name} beerImg={beer.image_url} beerTag={beer.tagline}/>
+    ));
+
+    setBeers(cardListJSX)
+  }
+
+  const handleHighAcidity = async () => {
+    const res = await fetch("https://api.punkapi.com/v2/beers");
+    const data = await res.json();
+
+    const newArr = data.filter((beer) => {
+        if (beer.ph > 4) {
           return(setBeers(<CardList key={beer.id} beerName={beer.name} beerImg={beer.image_url} beerTag={beer.tagline}/>))
         };
         return(null)
@@ -40,6 +75,7 @@ function App() {
 
       <nav className='nav'>
         <SearchBar getBeers={getBeers} filterResults={filterResults}></SearchBar>
+        <FiltersList handleHighAlcohol={handleHighAlcohol} handleClassicRange={handleClassicRange} handleHighAcidity={handleHighAcidity}></FiltersList>
       </nav>
       
       <div className="CardList-container">
--- src/components/FiltersList/FiltersList.jsx
+++ src/components/FiltersList/FiltersList.jsx
@@ -1,28 +1,17 @@
-import "./ListItem.scss";
-import { useState } from "react";
+import "./FiltersList.scss";
 
-const FiltersList = ({ listName, handleDelete }) => {
-  const [done, setDone] = useState("listItem");
-
-  const handleCheckbox = () => {
-    if (done == "listItem") {
-        setDone("listItem--done")
-    }
-    else{
-        setDone("listItem")
-    }
-  }
-
-  // https://api.punkapi.com/v2/beers?abv_gt=6
+const FiltersList = ({handleHighAlcohol, handleClassicRange, handleHighAcidity}) => {
 
   return (
     <>
-      <label htmlFor="">Greater than 6%</label>
-      <input type="checkbox" onClick={handleCheckbox} />
-      <label htmlFor="">Calssic</label>
-      <input type="checkbox" onClick={handleCheckbox} />
-      <label htmlFor="">Greater than 6%</label>
-      <input type="checkbox" onClick={handleCheckbox} />
+      <label htmlFor="">High Alcohol (ABV value greater than 6%)</label>
+      <input type="checkbox" onClick={handleHighAlcohol} />
+      <br />
+      <label htmlFor="">Classic Range (Was first brewed before 2010)</label>
+      <input type="checkbox" onClick={handleClassicRange} />
+      <br />
+      <label htmlFor="">High Acidity (pH lower than 4)</label>
+      <input type="checkbox" onClick={handleHighAcidity} />
     </>
    
   );
